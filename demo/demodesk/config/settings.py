80de54bcf1089fdf308d738f22380915aa3c6281
Brian Hendriks <brian@dolthub.com>
2025-06-25 11:57:53 -0700

    Updated config settings to use mysql instead of sqlite3

--- demo/demodesk/config/settings.py
+++ demo/demodesk/config/settings.py
@@ -138,8 +138,12 @@ LOGIN_REDIRECT_URL = "helpdesk:home"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'helpdesk',
+        'USER': 'helpdesk',
+        'PASSWORD': 'HelpDesk-pAsS',
+        'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
